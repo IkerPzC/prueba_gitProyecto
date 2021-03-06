36dcad0fc485b995319bd7a715efb770c8a042e9
ikerp <ikerperezcarcamo1921@gmail.com>
2021-03-06 12:57:31 +0100

    Inicio

---
+++ gitProyecto/src/gitProyecto/Calculadora.java
@@ -0,0 +1,19 @@
+package gitProyecto;
+
+public class Calculadora {
+	int num1, num2;
+	
+	public Calculadora(int num1, int num2){
+		this.num1=num1;
+		this.num2=num2;
+	}
+	
+	public int sumar(){
+		return num1+num2;
+	}
+	
+//	public static void main(String[] args) {
+//		Calculadora c=new Calculadora(1,2);
+//		System.out.println(c.sumar());
+//	}
+}
